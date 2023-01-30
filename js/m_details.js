9de32255e0899c4706e29d0ff0503b99f6595312
Narzullo <116525354+pNarzullo@users.noreply.github.com>
2023-01-30 20:36:06 +0500

    changing the readme file and others

--- js/m_details.js
+++ js/m_details.js
@@ -1,16 +1,13 @@
-$("img.img-svg").each(function () {
-    var $img = $(this);
-    var imgClass = $img.attr("class");
-    var imgURL = $img.attr("src");
-    $.get(imgURL, function (data) {
-        var $svg = $(data).find("svg");
-        if (typeof imgClass !== "undefined") {
-            $svg = $svg.attr("class", imgClass + " replaced-svg");
-        }
-        $svg = $svg.removeAttr("xmlns:a");
-        if (!$svg.attr("viewBox") && $svg.attr("height") && $svg.attr("width")) {
-            $svg.attr("viewBox", "0 0 " + $svg.attr("height") + " " + $svg.attr("width"))
-        }
-        $img.replaceWith($svg);
-    }, "xml");
+document.getElementById('review_forms').style.display = 'none';
+
+function review_forms() {
+  document.getElementById('review_forms').style.display = 'block';
+}
+
+const button = document.querySelector('button');
+const form = document.querySelector('#review_forms');
+
+button.addEventListener('click', () => {
+  form.classList.add('open');
 });
+
